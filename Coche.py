8799fd230a8172899e5123833db2cdf570a16f48
Luis Hidalgo Santaella <a21hisalu@iesgrancapitan.org>
2024-06-12 09:03:19 +0200

    Documentacion 1 - Luis Hidalgo Santaella

--- Coche.py
+++ Coche.py
@@ -20,6 +20,11 @@ class Coche(Vehiculo):
     cilindrada = 1000;
 
     def __init__(self):
+        """
+        Autor: Luis Hidalgo Santaella
+        Este es el constructor de la clase coche y devuelve un coche
+        :rtype: object
+        """
         pass;
 
     def __init__(self, num_serie, cilindrada, fabricante, color):
@@ -30,6 +35,11 @@ class Coche(Vehiculo):
 
     @property
     def num_serie(self):
+        """
+        Autor: Luis Hidalgo Santaella
+        Este es el metodo que hace referencia al número de serie y devuelve una cadena de números
+        :rtype: object
+        """
         return self.__num_serie
 
     @num_serie.setter
@@ -38,6 +48,11 @@ class Coche(Vehiculo):
 
     @property
     def color(self):
+        """
+        Autor: Luis Hidalgo Santaella
+        Este metodo hace referencia al color del coche y devuelve un color
+        :rtype: object
+        """
         return self.__color
 
     @color.setter
@@ -46,6 +61,11 @@ class Coche(Vehiculo):
 
     @property
     def cilindrada(self):
+        """
+        Autor: Luis Hidalgo Santaella
+        Este metodo hace referencia a la cilindrada y devuelve un valor
+        :rtype: object
+        """
         return self.__cilindrada
 
     @cilindrada.setter
@@ -54,6 +74,11 @@ class Coche(Vehiculo):
 
     @property
     def fabricante(self):
+        """
+        Autor: Luis Hidalgo Santaella
+        Este metodo hace referencia al fabricante y devuelve un valor
+        :rtype: object
+        """
         return self.__fabricante
 
     @fabricante.setter
--- Perro.py
+++ Perro.py
@@ -6,6 +6,11 @@ Autor: Jaime Rabasco Ronda.
 class Perro:
 
     def ladrar(self):
+        """
+        Autor: Luis Hidalgo Santaella.
+        Este metodo es para que un perro ladre y devuelve el ladrido del perro
+        :rtype: object
+        """
         self.guau = 'Guau'
         print(self.guau);
 
