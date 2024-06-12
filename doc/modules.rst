3e3924fbed586c6d73921baccaa09a567bc7f466
Luis Hidalgo Santaella <a21hisalu@iesgrancapitan.org>
2024-06-12 09:25:48 +0200

    Documentacion 2 - Luis Hidalgo santaella

---
+++ doc/modules.rst
@@ -0,0 +1,8 @@
+2023-24-examen-ordinaria-ud-4-entornos-12-06-2024-luishs03
+==========================================================
+
+.. toctree::
+   :maxdepth: 4
+
+   Coche
+   Perro
---
+++ doc/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Examen Entornos'
+copyright = '2024, Luis Hidalgo Santaella'
+author = 'Luis Hidalgo Santaella'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.imgmath',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'es'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
